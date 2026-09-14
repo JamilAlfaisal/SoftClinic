a42f704086ae216f2dd789bc6a74e17a808d8ed0
JamilAlfaisal <elfayssalmwafa@gmail.com>
2026-09-14 08:44:28 +0300

    @@

--- src/app/shared/sidebar/sidebar.ts
+++ src/app/shared/sidebar/sidebar.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import { NgClass } from '../../../../node_modules/@angular/common/types/_common_module-chunk';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,15 +9,23 @@ import { Component, signal } from '@angular/core';
   host: {
     '[class.isCollapsed]':'!drawerOpen()',
     '[class.isNotCollapsed]':'drawerOpen()'
-  }
+  },
+  imports: []
 })
 export class Sidebar {
 
+  
+
   drawerOpen = signal<boolean>(true);
+  selectedOption = signal<number>(1);
+
+  toggleReminder = signal<boolean>(false)
+  toggleUsers = signal<boolean>(false)
 
   toggleDrawer() {
     this.drawerOpen.update((prev) =>{
       return !prev
     })
   }
+
 }
